be90c59b58bc18b89354a5eb9808dfb0ec626b41
1030597 <rayane.silva@novomundo.com.br>
2022-06-23 21:37:43 -0300

    estudo sobre a rota

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,55 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/contato', function () {
-    return 'Contato';
+// Route::get('/contato', function () {
+//     return 'Contato';
+// });
+
+
+// Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+//      function(
+//         string $nome = 'teste nome', 
+//         string $categoria = 'teste categoria', 
+//         string $assunto = 'teste assunto', 
+//         string $mensagem = 'mensagem não informada') {
+//         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+// });
+
+//-- EXPRESSOES REGULARES ----
+
+// Route::get('/contato/{nome}/{categoria_id}',
+//       function(
+//          string $nome = 'teste nome', 
+//          int $categoria_id = 1
+//       ) {
+//          echo "Estamos aqui: $nome - $categoria_id ";
+//         }
+//  )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+
+Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
 
-Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal']);
-Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'sobreNos']);
-Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'contato']);
 
-Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
-     function(
-        string $nome = 'teste nome', 
-        string $categoria = 'teste categoria', 
-        string $assunto = 'teste assunto', 
-        string $mensagem = 'mensagem não informada') {
-        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1'); //redirecionamento pelo controller
+})->name('site.rota2');
+
+
+// Route::redirect('/rota2', '/rota1');   ---redirecionando a rota
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir a página inicial';
+
 });
-
-
